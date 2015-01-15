c3d04c57d6643c261308a3d1bf09319b57d9bf2c
yeming1001 <yeming_1001@163.com>
2015-01-15 22:07:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yeming/firstproject/Topbar.java
@@ -0,0 +1,31 @@
+package com.example.yeming.firstproject;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * author   YeMing(yeming_1001@163.com)
+ * Date:    2014-12-16 20:33
+ * version: V1.0
+ * Description:
+ */
+
+public class Topbar extends RelativeLayout {
+
+
+    public Topbar(Context context) {
+        super(context);
+    }
+    public Topbar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    /**
+     * @param s
+     */
+    public void test(String s){
+
+    }
+}
